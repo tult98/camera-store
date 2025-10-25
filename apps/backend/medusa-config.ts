23d46a64b9336e29b6c123588b98706284d17562
Tu Le <tu.le@employmenthero.com>
2025-10-25 21:21:07 +0700

    refactor: enable s3 for local development and update nextJS configurations

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -28,29 +28,27 @@ module.exports = defineConfig({
     {
       resolve: './src/modules/brand',
     },
-    // Only include file module in production
-    ...(process.env['NODE_ENV'] === 'production'
-      ? [
+    {
+      resolve: '@medusajs/file',
+      options: {
+        providers: [
           {
-            resolve: '@medusajs/file',
+            resolve: '@medusajs/file-s3',
+            id: 's3',
             options: {
-              providers: [
-                {
-                  resolve: '@medusajs/file-s3',
-                  id: 's3',
-                  options: {
-                    file_url: process.env['S3_FILE_URL'],
-                    access_key_id: process.env['S3_ACCESS_KEY_ID'],
-                    secret_access_key: process.env['S3_SECRET_ACCESS_KEY'],
-                    region: process.env['S3_REGION'],
-                    bucket: process.env['S3_BUCKET'],
-                    endpoint: process.env['S3_ENDPOINT'],
-                  },
-                },
-              ],
+              file_url: process.env.S3_FILE_URL,
+              access_key_id: process.env.S3_ACCESS_KEY_ID,
+              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+              region: process.env.S3_REGION,
+              bucket: process.env.S3_BUCKET,
+              endpoint: process.env.S3_ENDPOINT,
+              additional_client_config: {
+                forcePathStyle: true,
+              },
             },
           },
-        ]
-      : []),
+        ],
+      },
+    },
   ],
 });
--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -26,104 +26,16 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-      },
-      {
-        protocol: "https",
-        hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "medusa-server-testing.s3.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "fujifilm-x.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "i.postimg.cc",
-      },
-      {
-        protocol: "https",
-        hostname: "picsum.photos",
-      },
-      {
-        protocol: "https",
-        hostname: "fastly.picsum.photos",
-      },
-      {
-        protocol: "https",
-        hostname: "product.hstatic.net",
-      },
-      {
-        protocol: "https",
-        hostname: "fujifilmshop.vn",
-      },
-      {
-        protocol: "https",
-        hostname: "shopusa.fujifilm-x.com",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.vjshop.vn",
-      },
-      {
-        protocol: "https",
-        hostname: "images2.jessops.com",
-      },
-      {
-        protocol: "https",
-        hostname: "img.photographyblog.com",
-      },
-      {
-        protocol: "https",
-        hostname: "tokyocamera.vn",
-      },
-      {
-        protocol: "https",
-        hostname: "thecamerastore.com",
-      },
-      {
-        protocol: "https",
-        hostname: "giangduydat.vn",
-      },
-      {
-        protocol: "https",
-        hostname: "leicavietnam.com",
-      },
-      {
-        protocol: "https",
-        hostname: "asset.fujifilm.com",
-      },
-      {
-        protocol: "https",
-        hostname: "t3.ftcdn.net",
-      },
-      {
-        protocol: "https",
-        hostname: "t4.ftcdn.net",
-      },
-      {
-        protocol: "https",
-        hostname: "i.pinimg.com",
-      },
-      {
         protocol: "https",
         hostname: "ph-camera.sgp1.digitaloceanspaces.com",
       },
       {
         protocol: "https",
         hostname: "ph-camera.sgp1.cdn.digitaloceanspaces.com",
+      },
+      {
+        protocol: "https",
+        hostname: "bucket-fdsj-staging.up.railway.app",
       },
     ],
   },
